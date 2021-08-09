f45188f0158139952a4439f5fe0677b3f425699a
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2021-08-09 00:30:45 -0300

    Implementar inimigos com lógica simples
    Implementou-se um conjunto de entidades inimigas que perseguem o jogador
    usando o mesmo caminho gerado pelo BFS
    
    A classe Player foi retrabalhada, retirando-se as constantes definidas
    internamente com "números mágicos" e criando métodos que possibilitem
    que essa classe possa ser herdada.
    
    Implementou-se também uma lógica simples de detecção de derrota do
    jogador ao colidir com um inimigo

--- src/game/Panel.java
+++ src/game/Panel.java
@@ -12,10 +12,12 @@ import java.util.List;
 
 import javax.swing.JPanel;
 
+import game.entities.Enemy;
+import game.entities.Player;
 import graphs.GraphMatrix;
 import graphs.Position;
 
-public class Panel extends JPanel implements Runnable, MouseListener, MouseMotionListener{
+public class Panel extends JPanel implements Runnable, MouseListener, MouseMotionListener {
 
 	private static final long serialVersionUID = 1L;
 	private static int WIDTH = 500;
@@ -28,20 +30,31 @@ public class Panel extends JPanel implements Runnable, MouseListener, MouseMotio
 	private int lastMouseX;
 	private int lastMouseY;
 	private boolean inPlayer;
-	
+
+	private List<Enemy> enemies = new ArrayList<Enemy>();
+
+	final int playerMoves = 5;
+
+	final int enemyMoves = 3;
+
+	int rounds = 0;
+
 	public Panel() {
 		setFocusable(true);
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		addMouseListener(this);
 		addMouseMotionListener(this);
-		
-		player = new Player(5);
+
+		player = new Player(playerMoves, 5, 5, 25, 6, 14, 14, Color.BLUE);
+		enemies.add(new Enemy(enemyMoves, 10, 10, 25, 6, 10, 10, Color.RED));
+		enemies.add(new Enemy(enemyMoves, 15, 15, 25, 6, 10, 10, Color.RED));
+		enemies.add(new Enemy(enemyMoves, 10, 15, 25, 6, 10, 10, Color.RED));
 		grid = new GraphMatrix<Integer>(20, 20, 0, 1, -1);
 		preview = new ArrayList<Position>();
-		
+
 		start();
 	}
-	
+
 	private void start() {
 		running = true;
 		thread = new Thread(this);
@@ -51,34 +64,40 @@ public class Panel extends JPanel implements Runnable, MouseListener, MouseMotio
 	public void paint(Graphics g2) {
 		Graphics2D g;
 		g = (Graphics2D) g2;
-		
-		//Desenha a Grade (Possivelmente alterar para uma classe)
+
+		// Desenha a Grade (Possivelmente alterar para uma classe)
 		g.clearRect(0, 0, WIDTH, HEIGHT);
 		g.setColor(Color.GREEN);
 		g.fillRect(0, 0, WIDTH, HEIGHT);
 		g.setColor(Color.BLACK);
-		int Wsize = WIDTH/20;
-		int Hsize = HEIGHT/20;
-		for(int i=0;i<=WIDTH; i+= Wsize) {
+		int Wsize = WIDTH / 20;
+		int Hsize = HEIGHT / 20;
+		for (int i = 0; i <= WIDTH; i += Wsize) {
 			g.drawLine(i, 0, i, WIDTH);
 		}
-		for(int i=0;i<=HEIGHT; i+= Hsize) {
+		for (int i = 0; i <= HEIGHT; i += Hsize) {
 			g.drawLine(0, i, HEIGHT, i);
 		}
 
 		// Desenha as linhas de caminho
 		drawPreview(g);
-		
+
 		// Desenha o Jogador
 		player.draw(g);
+
+		// Desenha inimigos
+		for (Enemy enemy : enemies) {
+			enemy.draw(g);
+		}
 	}
 
 	@Override
 	public void run() {
-		while(running) {
+		while (running) {
 			repaint();
 		}
 	}
+
 	public void stop() {
 		running = false;
 		try {
@@ -86,6 +105,8 @@ public class Panel extends JPanel implements Runnable, MouseListener, MouseMotio
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		
+		System.out.println("Rounds " + rounds);
 	}
 
 	@Override
@@ -97,12 +118,12 @@ public class Panel extends JPanel implements Runnable, MouseListener, MouseMotio
 		// Coordenadas atuais do mouse na grade
 		int mx = coordToGrid(m.getX());
 		int my = coordToGrid(m.getY());
-		
+
 		// Atualiza as coordenadas do Mouse
-		if (lastMouseX != mx || lastMouseY != my){
+		if (lastMouseX != mx || lastMouseY != my) {
 			lastMouseX = mx;
 			lastMouseY = my;
-			if (mx == player.getGridX() && my == player.getGridY()) 
+			if (mx == player.getGridX() && my == player.getGridY())
 				inPlayer = true;
 			else {
 				inPlayer = false;
@@ -115,9 +136,19 @@ public class Panel extends JPanel implements Runnable, MouseListener, MouseMotio
 	public void mouseClicked(MouseEvent m) {
 		// Move o Jogador(Temporário)
 		if (preview.size() <= player.getMoves()) {
-		player.setGridX((m.getX()-1)/25);
-		player.setGridY((m.getY()-1)/25);
-		inPlayer = true;
+			player.setGridX((m.getX() - 1) / 25);
+			player.setGridY((m.getY() - 1) / 25);
+			inPlayer = true;
+
+			encontraCaminhoInimigos();
+			
+			rounds++;
+		}
+		
+		for (Enemy enemy : enemies) {
+			if (enemy.getGridX().equals(player.getGridX()) && enemy.getGridY().equals(player.getGridY())) {
+				stop();
+			}
 		}
 	}
 
@@ -140,34 +171,53 @@ public class Panel extends JPanel implements Runnable, MouseListener, MouseMotio
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
 	}
-	
+
 	private void drawPreview(Graphics2D g) {
 		int x = -1;
 		int y = -1;
 		int counter = 0;
 		g.setColor(Color.RED);
-		if(!preview.isEmpty() && !inPlayer) {
-			for(Position e : preview) {
-				if(counter >= player.getMoves())
+		if (!preview.isEmpty() && !inPlayer) {
+			for (Position e : preview) {
+				if (counter >= player.getMoves())
 					break;
-				if(x != -1 && y != -1)
-					g.drawLine(gridToCoord(e.getPosX())+12, gridToCoord(e.getPosY())+12, gridToCoord(x)+12, gridToCoord(y)+12);
+				if (x != -1 && y != -1)
+					g.drawLine(gridToCoord(e.getPosX()) + 12, gridToCoord(e.getPosY()) + 12, gridToCoord(x) + 12,
+							gridToCoord(y) + 12);
 				x = e.getPosX();
 				y = e.getPosY();
 				counter++;
 			}
 		}
 	}
-	
+
 	private void encontraCaminho() {
-		preview = grid.bfs(new Position(player.getGridX(),player.getGridY()), new Position(lastMouseX,lastMouseY));
+		preview = grid.bfs(new Position(player.getGridX(), player.getGridY()), new Position(lastMouseX, lastMouseY));
 		grid.clearMatrix();
 	}
-	
+
+	private void encontraCaminhoInimigos() {
+		for (Enemy enemy : enemies) {
+			List<Position> caminho = grid.bfs(new Position(enemy.getGridX(), enemy.getGridY()),
+					new Position(player.getGridX(), player.getGridY()));
+			if (!caminho.isEmpty()) {
+				if (caminho.size() > enemy.getMoves()) {
+					enemy.setGridX(caminho.get(enemy.getMoves()).getPosX());
+					enemy.setGridY(caminho.get(enemy.getMoves()).getPosY());
+				} else {
+					enemy.setGridX(caminho.get(caminho.size() - 1).getPosX());
+					enemy.setGridY(caminho.get(caminho.size() - 1).getPosY());
+				}
+			}
+			grid.clearMatrix();
+		}
+	}
+
 	private int gridToCoord(int v) {
 		return v * 25;
 	}
+
 	private int coordToGrid(int v) {
-		return (v-1)/25;
+		return (v - 1) / 25;
 	}
 }
